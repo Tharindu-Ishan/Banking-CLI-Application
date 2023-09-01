d4df63268a946ef9bd22e0ddcf343ae52429d933
Tharindu Ishan <ishanthari96@gmail.com>
2023-09-01 22:18:51 +0530

    Add readme and license files

--- Bank.java
+++ Bank.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Assignment07{
+public class Bank{
     private static final Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
         final String CLEAR = "\033[H\033[2J";
